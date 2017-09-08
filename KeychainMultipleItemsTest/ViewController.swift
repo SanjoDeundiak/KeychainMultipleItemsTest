ec4eb3d467e6dc1fede08f61338ae49732281bbd
Oleksandr Deundiak <Deundiak@gmail.com>
2017-09-08 13:11:23 +0300

    Fixed compilation error

--- KeychainMultipleItemsTest/ViewController.swift
+++ KeychainMultipleItemsTest/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        KeychainWrapper().addOneItem()
-        KeychainWrapper().addTwoItems()
+        KeychainWrapper().addAndCheckOneItem()
+        KeychainWrapper().addAndCheckTwoItems()
     }
 
     override func didReceiveMemoryWarning() {
